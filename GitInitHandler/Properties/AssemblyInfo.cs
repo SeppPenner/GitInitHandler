3045620f2f899ccbbe10197c417154d2cbb0fc82
tim <jan-brenner@gmx.de>
2017-03-15 17:48:53 +0100

    Initial commit.

---
+++ GitInitHandler/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("GitInitHandler")]
+[assembly: AssemblyDescription("GitInitHandler")]
+[assembly: AssemblyConfiguration("GitInitHandler")]
+[assembly: AssemblyCompany("Hämmer Electronics")]
+[assembly: AssemblyProduct("GitInitHandler")]
+[assembly: AssemblyCopyright("Copyright © Hämmer Electronics")]
+[assembly: AssemblyTrademark("GitInitHandler")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("bc4bbc64-ccaf-47c6-a7b3-b41db5e13008")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ README.md
@@ -0,0 +1,12 @@
+GitInitHandler
+===============
+
+GitInitHandler is an executable to run the 'git init' command when a file from the concerned folder is drag'n'dropped onto the exe.
+The executable is written and tested in .Net 4.6.2.
+
+[![Build status](https://ci.appveyor.com/api/projects/status/qxlolyonkcf53ouj?svg=true)](https://ci.appveyor.com/project/SeppPenner/gitinithandler)
+
+Change history
+--------------
+
+* **Version 1.0.0.0 (2017-03-15)** : 1.0 release.
