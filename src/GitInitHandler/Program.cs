f9740b90268d5b51677aae97c55e265cb8e272c1
SeppPenner <HansM2013@gmx.de>
2022-02-15 15:51:09 +0100

    Added nullable checks, added editorconfig, added file scoped namespaces, added global usings.

--- src/GitInitHandler/Program.cs
+++ src/GitInitHandler/Program.cs
@@ -1,4 +1,4 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="Program.cs" company = "Hämmer Electronics" >
 //   Copyright (c) All rights reserved.
 // </copyright>
@@ -7,63 +7,58 @@
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
-namespace GitInitHandler
+namespace GitInitHandler;
+
+/// <summary>
+/// The main program.
+/// </summary>
+public static class Program
 {
-    using System;
-    using System.Diagnostics;
-    using System.IO;
-
     /// <summary>
-    /// The main program.
+    /// The main method.
     /// </summary>
-    public static class Program
+    /// <param name="args">The arguments.</param>
+    public static void Main(string[] args)
     {
-        /// <summary>
-        /// The main method.
-        /// </summary>
-        /// <param name="args">The arguments.</param>
-        public static void Main(string[] args)
+        try
         {
-            try
+            if (args.Length == 0)
             {
-                if (args.Length == 0)
-                {
-                    return;
-                }
-
-                var path = Path.GetDirectoryName(args[0]);
-                var process = new Process();
-
-                if (path == null)
-                {
-                    return;
-                }
-
-                var startInfo = new ProcessStartInfo
-                {
-                    WorkingDirectory = path,
-                    WindowStyle = ProcessWindowStyle.Normal,
-                    FileName = "cmd.exe",
-                    RedirectStandardInput = true,
-                    UseShellExecute = false
-                };
-
-                process.StartInfo = startInfo;
-                process.Start();
-
-                using var writer = process.StandardInput;
-                if (!writer.BaseStream.CanWrite)
-                {
-                    return;
-                }
-
-                writer.WriteLine("git init");
+                return;
             }
-            catch (Exception ex)
+
+            var path = Path.GetDirectoryName(args[0]);
+            var process = new Process();
+
+            if (path == null)
             {
-                Console.WriteLine(ex.ToString());
-                Console.ReadKey();
+                return;
             }
+
+            var startInfo = new ProcessStartInfo
+            {
+                WorkingDirectory = path,
+                WindowStyle = ProcessWindowStyle.Normal,
+                FileName = "cmd.exe",
+                RedirectStandardInput = true,
+                UseShellExecute = false
+            };
+
+            process.StartInfo = startInfo;
+            process.Start();
+
+            using var writer = process.StandardInput;
+            if (!writer.BaseStream.CanWrite)
+            {
+                return;
+            }
+
+            writer.WriteLine("git init");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+            Console.ReadKey();
         }
     }
 }
